c6b36b44d2280455be4974305414fb778e5ea2e1
Göran Roseen <goran@roseen.se>
2021-10-22 16:10:52 +0200

    Add project files.

---
+++ RelayDiscovery/Controllers/RelaysController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace RelayDiscovery.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RelaysController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IEnumerable<Relay> Get()
+        {
+            return GetRelays().ConfigureAwait(false).GetAwaiter().GetResult();
+            /*
+            return new RelayDTO[] { 
+                new RelayDTO() { 
+                    Location = new Location() { City = "Stockholm", Continent = "EU", Country="Sweden" },
+                    Url = "https:" + "//relays.syncthing.net//120.29.217.52:443/?id=GJWNUPC-JEQFN6Q-3GTINBB-LC437KR-L3G4IXB-WD6ESGL-W4MXW6I-ISIT5A3"
+                } 
+            }; */
+        }
+
+        private static async Task<IEnumerable<Relay>> GetRelays()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "Relay filter");
+
+            var stringData = await client.GetStringAsync("https://relays.syncthing.net/endpoint");
+            var result = JsonSerializer.Deserialize<GetRelayResult>(stringData);
+            //return result.Relays;
+
+            // Filter & Sort
+//            var filteredResult = result.Relays.Where(r => r.Location?.Continent == "EU" && PortFromUrl(r.Url) == 443).OrderByDescending(r => r.Stats?.UptimeSeconds).ToList();
+            var filteredResult = result.Relays.Where(r => r.Location?.Continent == "EU" && PortFromUrl(r.Url) == 443).OrderByDescending(r => r.Stats?.Kbps10s1m5m15m30m60m.Last()).ToList();
+
+            return filteredResult;
+        }
+
+        private static int PortFromUrl(string url)
+        {
+            var uri = new Uri(url);
+            return uri.Port;
+        }
+    }
+}
---
+++ RelayDiscovery/Relay.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RelayDiscovery
+{
+    public class GetRelayResult
+    {
+        [JsonPropertyName("relays")]
+        public IEnumerable<Relay> Relays { get; set; }
+    }
+
+    public class Relay
+    {
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("location")]
+        public Location Location { get; set; }
+
+        [JsonPropertyName("stats")]
+        public Stats Stats { get; set; }
+
+        [JsonPropertyName("statsRetrieved")]
+        public string StatsRetrieved { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class Location
+    {
+        [JsonPropertyName("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("continent")]
+        public string Continent { get; set; }
+    }
+
+    public class Options
+    {
+        [JsonPropertyName("network-timeout")]
+        public int NetworkTimeout { get; set; }
+
+        [JsonPropertyName("ping-interval")]
+        public int PingInterval { get; set; }
+
+        [JsonPropertyName("message-timeout")]
+        public int MessageTimeout { get; set; }
+
+        [JsonPropertyName("per-session-rate")]
+        public int PerSessionRate { get; set; }
+
+        [JsonPropertyName("global-rate")]
+        public int GlobalRate { get; set; }
+
+        [JsonPropertyName("pools")]
+        public List<string> Pools { get; set; }
+
+        [JsonPropertyName("provided-by")]
+        public string ProvidedBy { get; set; }
+    }
+
+    public class Stats
+    {
+        [JsonPropertyName("startTime")]
+        public string StartTime { get; set; }
+
+        [JsonPropertyName("uptimeSeconds")]
+        public int UptimeSeconds { get; set; }
+
+        [JsonPropertyName("numPendingSessionKeys")]
+        public int NumPendingSessionKeys { get; set; }
+
+        [JsonPropertyName("numActiveSessions")]
+        public int NumActiveSessions { get; set; }
+
+        [JsonPropertyName("numConnections")]
+        public int NumConnections { get; set; }
+
+        [JsonPropertyName("numProxies")]
+        public int NumProxies { get; set; }
+
+        [JsonPropertyName("bytesProxied")]
+        public long BytesProxied { get; set; }
+
+        [JsonPropertyName("goVersion")]
+        public string GoVersion { get; set; }
+
+        [JsonPropertyName("goOS")]
+        public string GoOS { get; set; }
+
+        [JsonPropertyName("goArch")]
+        public string GoArch { get; set; }
+
+        [JsonPropertyName("goMaxProcs")]
+        public int GoMaxProcs { get; set; }
+
+        [JsonPropertyName("goNumRoutine")]
+        public int GoNumRoutine { get; set; }
+
+        [JsonPropertyName("kbps10s1m5m15m30m60m")]
+        public List<int> Kbps10s1m5m15m30m60m { get; set; }
+
+        [JsonPropertyName("options")]
+        public Options Options { get; set; }
+    }
+}
